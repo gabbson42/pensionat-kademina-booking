04e03179f8f2975f38018429ad4d329305a15546
Gabriel Landvik Åkerman <gabriel.landvikakerman@gmail.com>
2026-05-20 15:28:20 +0200

    Skapat tomma klasser och interface för att mappstrukturen ska upp på git

---
+++ src/main/java/org/example/pensionatkademina/controller/CustomerController.java
@@ -0,0 +1,4 @@
+package org.example.pensionatkademina.controller;
+
+public class CustomerController {
+}
---
+++ src/main/java/org/example/pensionatkademina/repository/CustomerRepository.java
@@ -0,0 +1,4 @@
+package org.example.pensionatkademina.repository;
+
+public interface CustomerRepository {
+}
