b85de5fac4ec2563648ccab5c71976ce3ce75d2c
Gabriel Landvik Åkerman <gabriel.landvikakerman@gmail.com>
2026-05-21 13:28:25 +0200

    Skapat en klass för Customer och påbörjat klasserna för repo, controller och service.

--- src/main/java/org/example/pensionatkademina/repository/CustomerRepository.java
+++ src/main/java/org/example/pensionatkademina/repository/CustomerRepository.java
@@ -1,4 +1,7 @@
 package org.example.pensionatkademina.repository;
 
-public interface CustomerRepository {
+import org.example.pensionatkademina.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
--- src/main/java/org/example/pensionatkademina/service/impl/CustomerServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package org.example.pensionatkademina.service.impl;
-
-public class CustomerServiceImpl {
-}
