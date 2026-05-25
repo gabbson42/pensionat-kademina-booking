c98bbfc8d7aa694ad4e3963eb98d62a8b82095e1
Filip Kjellander <filip.kjellander@gmail.com>
2026-05-25 23:19:44 +0200

    added @Builder annotation to Booking.java

--- src/main/java/org/example/pensionatkademina/model/Booking.java
+++ src/main/java/org/example/pensionatkademina/model/Booking.java
@@ -2,10 +2,7 @@ package org.example.pensionatkademina.model;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -14,6 +11,7 @@ import java.time.LocalDate;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Booking {
 
     @Id
