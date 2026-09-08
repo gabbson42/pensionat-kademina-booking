4338067fec60f62d59407ef93a0338403338125f
Simon-Fredholm <simon.fredholm2@gmail.com>
2026-09-08 14:31:25 +0200

    Add succes/error message

--- src/main/java/org/example/pensionatkademina/controller/ReviewController.java
+++ src/main/java/org/example/pensionatkademina/controller/ReviewController.java
@@ -6,11 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class ReviewController {
 
     private final ReviewClient reviewClient;
+
     public ReviewController(final ReviewClient reviewClient) {
         this.reviewClient = reviewClient;
 
@@ -22,9 +24,16 @@ public class ReviewController {
     }
 
     @PostMapping("/reviews")
-    public String reviewsPost(@ModelAttribute("reviewDto") ReviewDto reviewDto) {
-        reviewClient.createReview(reviewDto);
-        return "redirect:/reviews";
+    public String reviewsPost(@ModelAttribute("reviewDto") ReviewDto reviewDto, Model model) {
+        try {
+            reviewClient.createReview(reviewDto);
+            model.addAttribute("successMessage", "Review has been sent! thank you for your visit!");
+            return "reviews";
+        }
+        catch(Exception e) {
+                model.addAttribute(  "errorMessage", "you can only review a room you have booked.");
+            return "reviews";
+        }
     }
 
 }
