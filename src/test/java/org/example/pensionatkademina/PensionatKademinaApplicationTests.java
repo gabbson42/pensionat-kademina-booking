b68fd83df84c5aa768d9c89656cd02d2b56e4880
Gabriel Landvik Åkerman <gabriel.landvikakerman@gmail.com>
2026-09-18 17:03:43 +0200

    updated application.properties for test and added testcontainer in PensionatKademinaApplicationTests. added dependecies in pom

--- src/test/java/org/example/pensionatkademina/PensionatKademinaApplicationTests.java
+++ src/test/java/org/example/pensionatkademina/PensionatKademinaApplicationTests.java
@@ -2,9 +2,22 @@ package org.example.pensionatkademina;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mysql.MySQLContainer;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Testcontainers
 class PensionatKademinaApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer db =
+            new MySQLContainer(
+                    "mysql:8");
 
     @Test
     void contextLoads() {
